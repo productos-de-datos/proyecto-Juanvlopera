27a9254daea3f1d8dfd221c9df372029fddd6fa1
juanvl0796 <jsvinal@unal.edu.co>
2022-07-04 04:02:31 -0500

    primera subida

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,22 +1,42 @@
 """
 Módulo de ingestión de datos.
 -------------------------------------------------------------------------------
+Se descargan los datos y se guardan en la carpeta landing. 
 
 """
 
 
 def ingest_data():
-    """Ingeste los datos externos a la capa landing del data lake.
+     """Ingeste los datos externos a la capa landing del data lake.
 
     Del repositorio jdvelasq/datalabs/precio_bolsa_nacional/xls/ descarge los
     archivos de precios de bolsa nacional en formato xls a la capa landing. La
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import os
+    import pandas as pd
+    import xlwt
+    import openpyxl
+
+    def download_files(start_year, end_year, wdir, fpath):
+        for year_to_download in range (start_year, end_year):
+            try:
+                downloaded_file = pd.read_excel(wdir + '/' + str(year_to_download) + '.xlsx?raw=true')
+                downloaded_file.to_excel(fpath + str(year_to_download) + '.xlsx', index=None, header=True)
+            except:
+                downloaded_file = pd.read_excel(wdir + '/' + str(year_to_download) + '.xls?raw=true')
+                downloaded_file.to_excel(fpath + str(year_to_download) + '.xls', index=None, header=True)
+
+    fpath = 'data_lake/landing/'
+    start_year = 1995
+    end_year = 2022
+    wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/'
+    download_files(start_year,end_year,wdir,fpath)
+
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
+    ingest_data()
--- src/visualization/make_monthly_prices_plot.py
+++ src/visualization/make_monthly_prices_plot.py
@@ -1,42 +1,38 @@
 """
-Para realizar las gráfica de precios diarios
+Para realizar las gráfica de precios mensuales
 """
-def make_daily_prices_plot():
-    """Crea un grafico de lines que representa los precios promedios diarios.
 
+def make_monthly_prices_plot():
+    """Crea un grafico de lines que representa los precios promedios mensuales.
     Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
     lines que representa los precios promedios diarios.
-
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
-
     """
-    #raise NotImplementedError("Implementar esta función")
     import pandas as pd
     import matplotlib.pyplot as plt
+    from datetime import datetime
 
     def obtain_axix_plotting():
-        daily_prices = pd.read_csv('data_lake/business/precios-diarios.csv', header=0)
-        daily_prices['Fecha'] = pd.to_datetime(daily_prices['Fecha'])
-        X = daily_prices['Fecha']
-        y = daily_prices['Precio']
-        return X, y
-
-    def plotting(X,y):    
+        monthly_prices = pd.read_csv('data_lake/business/precios-mensuales.csv', header=0)
+        monthly_prices['Fecha'] = pd.to_datetime(monthly_prices['Fecha'], format='%Y-%m-%d')
+        X = monthly_prices['Fecha']
+        y = monthly_prices['Precio']
+        return X,y
+        
+    def plotting(X,y): 
         plt.figure(figsize=(15, 6))
         plt.plot(X, y, 'b', label='Prom. precio')
-        plt.title('Promedio de Precio Diario de Energía')
+        plt.title('Promedio de Precio Mensual de Energía')
         plt.xlabel('Fecha')
         plt.ylabel('Precio')
         plt.legend()
         plt.xticks(rotation="vertical")
-        plt.savefig("data_lake/business/reports/figures/daily_prices.png")
+        plt.savefig("data_lake/business/reports/figures/monthly_prices.png")
 
     X, y = obtain_axix_plotting()
     plotting(X, y)
 
-
 if __name__ == "__main__":
     import doctest
-    make_daily_prices_plot()
     doctest.testmod()
-
+    make_monthly_prices_plot()
